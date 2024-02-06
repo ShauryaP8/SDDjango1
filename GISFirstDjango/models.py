50f7091fef0c39fc0eccfb1a6b254fa034581b7c
ShauryaP8 <143507334+ShauryaP8@users.noreply.github.com>
2024-02-06 09:38:55 +0800

    Update models.py

--- GISFirstDjango/models.py
+++ GISFirstDjango/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    age = model.IntegerField(blank=False, null=True)
 
 class TeachingGroup(models.Model):
     name = models.CharField(max_length=30, null=True)
